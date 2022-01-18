411ccfbde04af289b5cf5c5d101e9a7f0154fc82
Kev. Noel <arita37@users.noreply.github.com>
2022-01-18 12:47:57 +0900

    Update run_test.py

--- build_hnsw/recipe/run_test.py
+++ build_hnsw/recipe/run_test.py
@@ -1,10 +1,26 @@
+print('start')
+import hnswlib
 import numpy as np
+import pickle
 
-import hnswlib        
-dim = 50
-elements = 10_000
+dim = 8
+num_elements = 10
 
-hnsw = hnswlib.Index(space='cosine', dim=dim)
+# Generating sample data
+data = np.float32(np.random.random((num_elements, dim)))
+ids = np.arange(num_elements)
 
+# Declaring index
+p = hnswlib.Index(space = 'l2', dim = dim) # possible options are l2, cosine or ip
+print(p)
 
+# Initializing index - the maximum number of elements should be known beforehand
+p.init_index(max_elements = num_elements, ef_construction = 200, M = 16)
+
+# Element insertion (can be called several times):
+p.add_items(data, ids)
                      
+print('passed')
+
+
+
